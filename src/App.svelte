38ca77693cce2336315b6ba1e336f8c726e3eafe
Burhan <2019cs626@student.uet.edu.pk>
2021-10-08 12:50:00 +0500

    Made the table

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,18 @@
 	export let name;
 </script>
 
-<div>
-
+<div class="h-screen flex justify-center items-center">
+	<div class="poker_table rounded-full w-4/5 h-4/6 border border-black" />
 </div>
+
 <style lang="postcss" global>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
+
+	.poker_table {
+		background-color: #35654d;
+		border: 20px solid black;
+		background: radial-gradient(#52c234, #061700);
+	}
 </style>
