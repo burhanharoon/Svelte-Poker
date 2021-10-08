ee343a10ca65dc3008a6d1c40537af1f7a5ae687
Burhan <2019cs626@student.uet.edu.pk>
2021-10-08 14:09:39 +0500

    Added card images

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-	export let name;
+	const cards = ["/images/back.svg"];
 </script>
 
 <div class="h-screen flex justify-center items-center">
-	<div class="poker_table rounded-full w-4/5 h-4/6 border border-black" />
+	<div class="poker_table rounded-full w-4/5 h-4/6 border border-black">
+		<div class="flex justify-center items-center">
+			<img src={cards[0]} alt="" />
+		</div>
+	</div>
 </div>
 
 <style lang="postcss" global>
---
+++ src/cards.js
@@ -0,0 +1,2 @@
+
+export const cards = ["/images/back.svg"]
